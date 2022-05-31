935c466956b610476d16f05d3bbe4b54c92bf754
Agshu <agnes.shutrick@gmail.com>
2022-05-31 15:15:32 +0200

    added the word mobile

--- src/demo.js
+++ src/demo.js
@@ -9,7 +9,7 @@ const demo = () => {
 
     <section className="demo-text">
       <h1>The Game</h1>
-      <p>Planting a seed is a game about the journey of a small seed to find a place for it to sprout. It  is a single player 3D Navigational game, in which the player guides the seed along its journey to the center of a planet. Along the way, the player must traverse multiple layers of the planet, making use of the ability to reverse the gravitational forces to navigate to the next layer. 
+      <p>Planting a seed is a mobile game about the journey of a small seed to find a place for it to sprout. It  is a single player 3D Navigational game, in which the player guides the seed along its journey to the center of a planet. Along the way, the player must traverse multiple layers of the planet, making use of the ability to reverse the gravitational forces to navigate to the next layer. 
       <br></br><br></br>
         On the path, the seed meets obstacles and hardship. To get to the center of the world, it has to get through the gates by collecting keys. However, the seed’s energy supply is in danger, and thus the player must stay active and think quickly to survive the journey!   </p>
     </section>
